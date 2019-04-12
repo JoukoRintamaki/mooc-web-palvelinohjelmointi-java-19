f33be62f2259e1f9b92022d52f295a660e89a457
Jouko Rintamäki <jouko.rintamaki@live.fi>
2019-04-12 18:39:49 +0300

    .

--- osa05-Osa05_01.HelloSession/src/main/java/hellosession/HelloSessionController.java
+++ osa05-Osa05_01.HelloSession/src/main/java/hellosession/HelloSessionController.java
@@ -1,16 +1,23 @@
 package hellosession;
 
 import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloSessionController {
-    
+
     @RequestMapping("*")
     @ResponseBody
-    public String sayHello() {
-        return "Hello there!";
+    public String sayHello(HttpSession httpSession) {
+        int visits = 0;
+        if (httpSession.getAttribute("count") != null) {
+            visits = (int) httpSession.getAttribute("count");
+        }
+        visits++;
+        httpSession.setAttribute("count", visits);
+        return visits == 1 ? "Hello there!" : "Hello again!";
     }
 }
--- osa05-Osa05_06.Reservations/src/test/java/reservations/ReservationTestUtilities.java
+++ osa05-Osa05_06.Reservations/src/test/java/reservations/ReservationTestUtilities.java
@@ -2,6 +2,7 @@ package reservations;
 
 import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +13,9 @@ public class ReservationTestUtilities {
 
     @Autowired
     private AccountRepository accountRepository;
+    
+    @Autowired
+    public PasswordEncoder passwordEncoder;
 
     public long reservationCount() {
         return reservationRepository.count();
@@ -27,7 +31,7 @@ public class ReservationTestUtilities {
         if (a == null) {
             a = new Account();
             a.setUsername(username);
-            a.setPassword("pw");
+            a.setPassword(passwordEncoder.encode("pw"));
 
             a = accountRepository.save(a);
         }
