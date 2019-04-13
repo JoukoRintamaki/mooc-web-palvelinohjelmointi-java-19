c4f224fefcf30068dd5a6cdcbda1d85c04ea6227
Jouko Rintamäki <jouko.rintamaki@live.fi>
2019-04-13 16:54:15 +0300

    .

--- osa05-Osa05_02.ReloadHeroes/src/main/java/reloadheroes/ReloadController.java
+++ osa05-Osa05_02.ReloadHeroes/src/main/java/reloadheroes/ReloadController.java
@@ -1,7 +1,5 @@
 package reloadheroes;
 
-import java.util.UUID;
-import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +7,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.Random;
+import java.util.UUID;
 
 @Controller
 public class ReloadController {
@@ -21,11 +23,22 @@ public class ReloadController {
 
     @RequestMapping("*")
     public String reload(Model model) {
-
-        model.addAttribute("status", new ReloadStatus());
-
-
-        model.addAttribute("scores", reloadStatusRepository.findAll());
+        if (session.getAttribute("reloadStatus") != null) {
+            ReloadStatus reloadStatus = (ReloadStatus) session.getAttribute("reloadStatus");
+            ReloadStatus reloadStatus1 = reloadStatusRepository.findByName(reloadStatus.getName());
+            reloadStatus1.setReloads(reloadStatus1.getReloads()+1);
+            reloadStatusRepository.save(reloadStatus1);
+            session.setAttribute("reloadStatus",reloadStatus1);
+            model.addAttribute("status", reloadStatus1);
+        } else {
+            ReloadStatus reloadStatus = new ReloadStatus(UUID.randomUUID().toString(), 1);
+            reloadStatusRepository.save(reloadStatus);
+            session.setAttribute("reloadStatus",reloadStatus);
+            model.addAttribute("status", reloadStatus);
+        }
+        Pageable pageable = PageRequest.of(0, 5, Sort.by("reloads").descending());
+        model.addAttribute("scores", reloadStatusRepository.findAll(pageable));
+        System.out.println(session);
         return "index";
     }
 }
